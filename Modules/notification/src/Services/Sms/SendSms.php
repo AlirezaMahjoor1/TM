d456a502105c8c2e82be71dfc10d4fcd2c452c1a
AlirezaMahjoor1 <Alireza.mahjoor@gmail.com>
2022-06-18 20:17:13 +0430

    add send sms function

--- Modules/notification/src/Services/Sms/SendSms.php
+++ Modules/notification/src/Services/Sms/SendSms.php
@@ -2,34 +2,37 @@
 
 namespace TM\Notification\Services\Sms;
 
+use Illuminate\Support\Facades\Http;
+
 class SendSms
 {
+    private $recipient;
+    private $message;
 
-    public static function send()
+    public function __construct($recipient, $message)
     {
-        $url = "https://ippanel.com/services.jspd";
-        $rcpt_nm = ['9121111111'];
-        $param = [
-            'uname' => '',
-            'pass' => '',
-            'from' => '',
-            'message' => 'تست',
-            'to' => json_encode($rcpt_nm),
-            'op' => 'send'
-        ];
-
-        $handler = curl_init($url);
-        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($handler, CURLOPT_POSTFIELDS, $param);
-        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
-        $response2 = curl_exec($handler);
-
-        $response2 = json_decode($response2);
-        $res_code = $response2[0];
-        $res_data = $response2[1];
-
-
-        echo $res_data;
+        $this->recipient = $recipient;
+        $this->message = $message;
     }
+
+    public function send()
+    {
+
+
+        $response = Http::withHeaders([
+            'Authorization' => 'AccessKey 1HiI_pXHJ2RZCFJ9ZaLrn-Oo_7bi2PmvnuXCm2aX2SQ='
+        ])->post("http://rest.ippanel.com/v1/messages", [
+
+            "originator" => "+9890000145",
+            "recipients" => [
+                "$this->recipient"
+            ],
+            "message" => "$this->message"
+
+        ]);
+        return ($response);
+
+    }
+
 
 }
